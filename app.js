8f022845cb2a0c9ba2b807140a2b5a2c17b39189
RaoudhaHamdi <raoudha2017@outlook.fr>
2018-11-04 09:29:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+$(document).ready(function ()
+{
+    
+$(window).scroll(function () {
+			if ($(this).scrollTop() != 0) {
+				$('#toTop').fadeIn();
+			} else {
+				$('#toTop').fadeOut();
+			}
+		});
+
+       $('#toTop').click(function()
+    {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });    
+})
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+
